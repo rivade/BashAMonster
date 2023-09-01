3116c0e249c8acde12c232688fea0adbe1cebc9d
theo lindqvist <theo.lindqvist@elev.ga.ntig.se>
2023-09-01 15:59:38 +0200

    Commit

---
+++ BashAMonster/Fish.cs
@@ -0,0 +1,109 @@
+﻿namespace BashAMonster;
+
+public class Enemy
+{
+    public int HP;
+    public string Name;
+    public int Damage;
+    public virtual void Attack() { }
+    public virtual void Draw() { }
+}
+
+public class Skeleton : Enemy
+{
+    Random generator = new();
+    public Skeleton(string inputName)
+    {
+        HP = 100;
+        Name = inputName;
+    }
+
+    public override void Attack()
+    {
+        Damage = generator.Next(10, 25);
+        HP -= Damage;
+        System.Console.WriteLine("(Sounds of bones cracking)");
+        System.Console.WriteLine($"You did {Damage} damage!");
+        if (HP <= 0)
+        {
+            System.Console.WriteLine("He dead xd");
+        }
+        else
+        {
+            System.Console.WriteLine($"He has {HP} health remaining");
+        }
+        System.Console.ReadKey();
+    }
+
+    public override void Draw()
+    {
+        System.Console.WriteLine(@"      .-.
+     (o.o)
+      |=|
+     __|__
+   //.=|=.\\
+  // .=|=. \\
+  \\ .=|=. //
+   \\(_=_)//
+    (:| |:)
+     || ||
+     () ()
+     || ||
+     || ||
+    ==' '==");
+        System.Console.WriteLine($"{Name}'s HP = {HP}");
+        System.Console.WriteLine("Press any key to attack!");
+        System.Console.WriteLine();
+        Console.ReadKey();
+        Attack();
+    }
+}
+
+public class Zombie : Enemy
+{
+    Random generator = new();
+    public Zombie(string inputName)
+    {
+        HP = 100;
+        Name = inputName;
+    }
+    public override void Attack()
+    {
+        Damage = generator.Next(5, 15);
+        HP -= Damage;
+        System.Console.WriteLine($"({Name} groans loudly)");
+        System.Console.WriteLine($"You did {Damage} damage!");
+        if (HP <= 0)
+        {
+            System.Console.WriteLine("He dead xd");
+        }
+        else
+        {
+            System.Console.WriteLine($"He has {HP} health remaining");
+        }
+        System.Console.ReadKey();
+    }
+
+    public override void Draw()
+    {
+        System.Console.WriteLine(@"                                (()))
+                               /|x x|
+                              /\( - )
+                      ___.-._/\/
+                     /=`_'-'-'/  !!
+                     |-{-_-_-}     !
+                     (-{-_-_-}    !
+                      \{_-_-_}   !
+                       }-_-_-}
+                       {-_|-_}
+                       {-_|_-}
+                       {_-|-_}
+                       {_-|-_}
+                   ____%%@ @%%_______");
+        System.Console.WriteLine($"{Name}'s HP = {HP}");
+        System.Console.WriteLine("Press any key to attack!");
+        System.Console.WriteLine();
+        Console.ReadKey();
+        Attack();
+    }
+}
---
+++ BashAMonster/Program.cs
@@ -0,0 +1,36 @@
+﻿using BashAMonster;
+Random generator = new();
+
+Skeleton SkellyJonas = new Skeleton("Jonas");
+Zombie ZombPeter = new Zombie("Peter");
+
+Enemy[] enemyArr = { SkellyJonas, ZombPeter };
+int currentEnemy = generator.Next(2);
+string answer;
+
+while (true)
+{
+    while (enemyArr[currentEnemy].HP > 0)
+    {
+        System.Console.Clear();
+        enemyArr[currentEnemy].Draw();
+    }
+    System.Console.Clear();
+    System.Console.WriteLine("Do you want to play again? y/n");
+    answer = System.Console.ReadLine();
+    while (answer != "y" && answer != "n")
+    {
+        System.Console.WriteLine("You have to write y or n");
+        System.Console.WriteLine("Do you want to play again? y/n");
+        answer = System.Console.ReadLine();
+    }
+    if (answer == "y")
+    {
+        enemyArr[currentEnemy].HP = 100;
+        currentEnemy = generator.Next(2);
+    }
+    else
+    {
+        break;
+    }
+}
